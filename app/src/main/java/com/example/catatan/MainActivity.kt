8cad6259280eee0ee79fa883a2ef77dd2582dab2
ahmadfma <57690612+arif200117@users.noreply.github.com>
2021-06-11 22:46:55 +0800

    masukkan inputan kedalam database ketika tombol simpan diklik

--- app/src/main/java/com/example/catatan/MainActivity.kt
+++ app/src/main/java/com/example/catatan/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.catatan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
+import com.example.catatan.User.CatatanEntity
 import com.example.catatan.User.UserViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +35,26 @@ class MainActivity : AppCompatActivity() {
                 .setView(dialogView)
         }
         val mDialog = builder?.show()
+        with(dialogView) {
+            hapusBtn.visibility = View.GONE
+            savebtn.setOnClickListener {
+                val judul = judulinput.text.toString()
+                val desc = deskripsiinput.text.toString()
+                val waktu = waktuInput.text.toString()
+                val lokasi = lokasiinput.text.toString()
+
+                if( judul != "" &&  desc != "" &&  waktu != "" && lokasi != "") {
+                    viewmodel.insertCatatan(
+                        CatatanEntity(
+                            0, judul, desc, waktu, lokasi
+                        )
+                    )
+                    mDialog?.dismiss()
+                    Toast.makeText(this@MainActivity, "Catatan Berhasil Ditambahkan", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Harap Mengisi Semua Kolom", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
